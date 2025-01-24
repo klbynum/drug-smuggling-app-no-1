a6f32b57f3983495dce2d265cc3d0a4b6f82ee4b
Kemon <kemonbynum18@gmail.com>
2025-01-24 13:57:01 -0500

    loginPageEdits complete

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,28 +5,27 @@ import { FaUserAlt, FaLock } from "react-icons/fa";
 
 const Login = () => {
   return (
-    <>
-    <div className='wrapper'> </div>
-      <form className="allInfo">
-        <h1>Sign in to TSA Smuggling Detection</h1>
+    <div className='wrapper'>
+        <form className="allInfo">
+        <h2>Sign in to TSA Smuggling Detection</h2>
         <div className="input-box">
             <input type="text" placeholder='Username' required />
             <FaUserAlt className='icon' />
         </div>
         
-        <div className='remember'>
+        <div className='input-box'>
             <input type="password" placeholder='Password' required />
             <FaLock className='icon' />
         </div>
 
         <div>
-            <label><input type = "checkbox" className="rememLabel"/>Remember me </label>
+            <label className="rememLabel">< input type = "checkbox"/>Remember me </label>
             <a href="#">Forgot password?</a>
         </div>
         <button type="submit">Login</button>
-    </form>
-    </>
-  )
-}
+      </form>
+    </div>  
+  );
+};
 
 export default Login
